b3e6a5d63238f49f2d00dc2020c9362381ee4019
Seth Dodson <seth_dodson@hotmail.com>
2011-08-14 17:13:13 -0400

    First commit

---
+++ Encounter Generator/Encounter Generator/Program.fs
@@ -0,0 +1,58 @@
+﻿// Learn more about F# at http://fsharp.net
+
+open System
+
+let random = new Random(DateTime.Now.Millisecond)
+
+let roll dice sides = 
+    Seq.initInfinite(fun index -> random.Next(1, (sides + 1)))
+    |> Seq.take(dice)
+    |> Seq.sum
+
+type Climate = 
+    | Plain
+    | Scrub
+    | Forest
+    | Rough
+    | Hill
+    | Mountain
+    | Swamp
+    | Desert
+    
+type Frequency = 
+    | Common
+    | Uncommon
+    | Rare
+    | VeryRare
+    
+type Organization = 
+    | Solitary
+    | Tribal
+    
+type Diet = 
+    | Carnivore 
+    | Herbivore
+    | Omnivore
+  
+[<Measure>] type Copper
+
+[<Measure>] type Silver
+
+[<Measure>] type Gold
+
+[<Measure>] type Electrum
+
+[<Measure>] type Platinum
+
+
+//type Treasure = 
+        
+    
+type ICreature = 
+    abstract member Climate : unit -> Climate
+    abstract member Frequency : unit -> Frequency
+    abstract member Organization : unit -> Organization
+    abstract member Diet : unit -> Diet
+    abstract member Intelligence : unit -> int
+    
+    
